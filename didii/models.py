9fb1f06db26c6730d9f846406ff7d5bdfc4ea612
thien2101 <thienb1910300@student.ctu.edu.vn>
2022-10-07 18:17:33 +0700

    7/10

--- didii/models.py
+++ didii/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class House(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+    
+class House_1(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
